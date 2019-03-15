ae8b1d80d7db01dee14ef93c04d21aae6e8821de
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-03-15 17:40:44 +0100

    Update srml/staking/src/lib.rs

--- srml/staking/src/lib.rs
+++ srml/staking/src/lib.rs
@@ -182,8 +182,13 @@
 //!     validator and their nominators is calculated by multiplying the result of the above point
 //!       by `2.pow(unstake_threshold)`. This punishment is shared in the same manner as the rewards.
 //! - If the previous overflows, then `slot_stake` is used. (NOTE: This should never happen in a correctly implemented, non-corrupted, well-configured system)
+<<<<<<< HEAD
 //! - All individual accounts' punishments are capped at their total stake. (NOTE: This cap should never comee into force in a correctly implemented, non-corrupted, well-configured system)
 //!
+=======
+//! - All individual accounts' punishments are capped at their total stake. (NOTE: This cap should never come into force in a correctly implemented, non-corrupted, well-configured system)
+//! 
+>>>>>>> 1fee935c... Update srml/staking/src/lib.rs
 //! ### Additional Fund Management Operations
 //!
 //! Any funds already placed into stash can be the target of the following operations:
